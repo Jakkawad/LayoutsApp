45d10c6a54c73b8ca0d80f14f71a41e5a176b738
Jakkawad <admin@all2sale-iMac.local>
2016-10-06 16:57:48 +0700

    Page 3 Layouts
    Page 3 Layouts

--- All2Sale/Page3/Page3MainViewController.swift
+++ All2Sale/Page3/Page3MainViewController.swift
@@ -10,21 +10,40 @@ import UIKit
 
 class Page3MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var titleArray = ["Shop name", "Product detail", "Price"]
+    
     @IBOutlet weak var tableView:UITableView!
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 5
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell0 = tableView.dequeueReusableCell(withIdentifier: tableCell0)
+        cell0?.textLabel?.text = titleArray[indexPath.row]
         return cell0!
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return CGFloat.leastNormalMagnitude
+        }
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 44
+        } else if indexPath.row == 1 {
+            return 200
+        } else {
+            return 100
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
